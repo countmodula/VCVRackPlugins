ab861b95d50dd7ca3d33a8ff47ddea0f6b988cf3
AdamVerspaget <49931541+AdamVerspaget@users.noreply.github.com>
2020-06-06 11:52:08 +1000

    Fix issue with extraneous screws appearing on 8 HP panels

--- src/components/stdScrews.hpp
+++ src/components/stdScrews.hpp
@@ -9,7 +9,7 @@ addChild(createWidget<CountModulaScrew>(Vec(RACK_GRID_WIDTH, 0)));
 addChild(createWidget<CountModulaScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 // add the right hand screws for modules wider than 8HP
-if (box.size.x / RACK_GRID_WIDTH > 8.0f) {
+if (box.size.x / RACK_GRID_WIDTH > 8.1f) {
 	addChild(createWidget<CountModulaScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 	addChild(createWidget<CountModulaScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 }
