039832147326d12d215982e305250bba16a92751
Adam Verspaget <adam-v@spiralsect.com>
2023-01-01 17:03:18 +1100

    #104 Random Access Switches not honoring theme settings in module browser

--- src/modules/RandomAccessSwitch18.cpp
+++ src/modules/RandomAccessSwitch18.cpp
@@ -197,9 +197,10 @@ struct RandomAccessSwitch18Widget : ModuleWidget {
 
 	RandomAccessSwitch18Widget(RandomAccessSwitch18 *module) {
 		setModule(module);
-
 		panelName = PANEL_FILE;
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/" + panelName)));
+
+		// set panel based on current default
+		#include "../themes/setPanel.hpp"	
 
 		// screws
 		#include "../components/stdScrews.hpp"
